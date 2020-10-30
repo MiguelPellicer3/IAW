9478a4dd269fc2a0dd5bb5faaca241759b4e2272
MiguelPellicer3 <63378@iessierradeguara.com>
2020-10-30 15:33:59 +0100

    final clase 28/10

--- web calculadoraDesplegable/app.js
+++ web calculadoraDesplegable/app.js
@@ -1,50 +1,38 @@
 function calcular() {
+    /*lectura de operandos y operacion*/
     let op1 = document.getElementById("op1");
     let op2 = document.getElementById("op2");
     let operacion = document.getElementById("operacion");
     let resultado;
     let parrafoResultado = document.getElementById("res");
-    if (operacion.value == "suma") {
-        resultado = suma(op1.value, op2.value);  
-    }else if (operacion.value == "resta"){
-        resultado=resta(op1.value, op2.value);
-    }else if (operacion.value == "multi"){
-        resultado=multi(op1.value, op2.value);
-    }
-    if (resultado != null) {
-        parrafoResultado.innerHTML= resultado;    
+    /* comprobar si los operandos son correctos*/
+    if(operandosCorrectos(op1.value, op2.value)){
+        /* realizar la operacion */
+        resultado = realizarOperacion(op1.value, op2.value, operacion.value);
+        /* imprimir el resultado */
+        parrafoResultado.innerHTML = resultado;
     }else{
-        parrafoResultado.innerHTML= "Error en los operandos";    
+        /* imprimir mensaje de error */
+        parrafoResultado.innerHTML = "Error en los operandos";
     }
 }
 
-
-function suma(o1, o2) {
-    let resultado=0;
-     if(isNaN(o1) == true ||  isNaN(o2) == true ){
-        resultado = null;
-     }else{
-        resultado = (parseFloat(o1) +  parseFloat(o2)).toFixed(2);
-     }
-    return resultado;
-}
-
-function resta(o1, o2) {
-    let resultado=0;
-    if(isNaN(o1) == true || isNaN(o2) == true ){
-        resultado = null;
+function operandosCorrectos(operando1, operando2) {
+    if (isNaN(operando1) == true || isNaN(operando2) == true){
+        return false;
     }else{
-        resultado = (parseFloat(o1) - parseFloat(o2)).toFixed(2);
+        return true;
     }
-    return resultado;
 }
 
-function multi(o1, o2) {
-    let resultado=0;
-    if(isNaN(o1) == true || isNaN(o2) == true ){
-        resultado = null;
-    }else{
-        resultado = (parseFloat(o1) * parseFloat(o2)).toFixed(2);
+function realizarOperacion(operando1, operando2, operacion) {
+    let resultado = null;
+    if (operacion == "suma") {
+        resultado=(parseFloat(operando1) + parseFloat(operando2)).toFixed(2);
+    } else if (operacion == "resta") {
+        resultado=(parseFloat(operando1) - parseFloat(operando2)).toFixed(2);
+    } else if (operacion == "multi"){
+        resultado=(parseFloat(operando1) * parseFloat(operando2)).toFixed(2);
     }
     return resultado;
 }
