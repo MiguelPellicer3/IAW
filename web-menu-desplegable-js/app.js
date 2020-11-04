5db28e7ee4c8e0788be16b139e917964f4cde2f3
Miguel clase <63378@iessierradeguara.com>
2020-11-04 17:37:27 +0100

    no entiendo por que no puedo pullear

---
+++ web-menu-desplegable-js/app.js
@@ -0,0 +1,45 @@
+let menuMostrado = false;
+
+function mostrarOcultarMenu() {
+    if (menuMostrado) {
+        ocultarMenu();
+    } else {
+        mostrarMenu();
+    }
+}
+
+
+function mostrarMenu() {
+    /* codigo para mostrar el menu */
+    let menu = document.getElementById("listaMenu");
+    console.log(menu);
+    for (let i = 0; i < menu.children.length; i++) {
+        menu.children[i].children[1].style.display = "inline";
+    }
+    /*menu.children[0].children[1].style.display = "inline";
+    menu.children[1].children[1].style.display = "inline";
+    menu.children[2].children[1].style.display = "inline";
+    menu.children[3].children[1].style.display = "inline";
+
+
+    /* ---- */
+    menuMostrado = true;
+}
+
+function ocultarMenu() {
+    /* codigo para ocultar el menu */
+    let menu = document.getElementById("listaMenu");
+    for (const liHijo of menu.children) {
+        liHijo.children[1].style.display = "none";
+    }
+
+    /*menu.children[0].children[1].style.display = "none";
+    menu.children[1].children[1].style.display = "none";
+    menu.children[2].children[1].style.display = "none";
+    menu.children[3].children[1].style.display = "none";
+
+
+
+    /* ---- */
+    menuMostrado = false;
+}
