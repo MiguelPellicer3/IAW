75ed21e662ef85e501148ca5638a66ec6a9b9959
MiguelPellicer3 <63378@iessierradeguara.com>
2020-11-11 18:11:20 +0100

    11/11

--- web-js-cronometro/app.js
+++ web-js-cronometro/app.js
@@ -5,6 +5,15 @@ function empezar() {
     objetoTimer = setInterval(incrementar, 1000);
 }
 
+function empezarconCuentaAtras() {
+    new Promise(function cuentaAtras() {
+        setTimeout(function cuentaAtras() {
+            // Crear cuenta atras y cuando finalice inicializar la funcion empezar
+
+        })
+    })
+}
+
 function reset() {
     document.getElementById("crono").innerHTML = 0;
     contador = 0;
