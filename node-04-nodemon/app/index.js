e1269d8d929107d521c7f94b11d9f5c9784aa27e
MiguelPellicer3 <63378@iessierradeguara.com>
2021-01-25 20:13:02 +0100

    25/1

--- node-04-nodemon/app/index.js
+++ node-04-nodemon/app/index.js
@@ -9,11 +9,19 @@ function (req,res) {
     let pathtoIndex = path.join(__dirname,"public" ,"index.html");
     res.sendFile(pathtoIndex);
 }
-)
+);
+app.get('/:nombre',
+function (req,res) { 
+    let nombreArchivo = req.params.nombre+".html";
+    let pathtoIndex = path.join(__dirname,"public", nombreArchivo);
+    res.sendFile(pathtoIndex);
+}
+); 
 
-app.listen(app.get("puerto"),inicioservidor(),
+
+app.listen(app.get("puerto"),inicioservidor());
 
     function inicioservidor() {
-    console.log('Servidor iniciado en:${app.get("puerto")}');
+    console.log(
+        `Servidor iniciado en http://localhost:${app.get("puerto")}`);
     }
-)
