129cd0980f000f8f1e43c1dc4603e49e4016db25
MiguelPellicer3 <63378@iessierradeguara.com>
2021-01-25 19:44:39 +0100

    menudo subnormal

--- node-04-nodemon/app/index.js
+++ node-04-nodemon/app/index.js
@@ -1,1 +1,19 @@
-console.log("hola mundoo");
+const express = require('express');
+const app = express();
+const path= require('path');
+
+app.set("puerto", 3000);
+
+app.get('/',
+function (req,res) {
+    let pathtoIndex = path.join(__dirname,"public" ,"index.html");
+    res.sendFile(pathtoIndex);
+}
+)
+
+app.listen(app.get("puerto"),inicioservidor(),
+
+    function inicioservidor() {
+    console.log('Servidor iniciado en:${app.get("puerto")}');
+    }
+)
