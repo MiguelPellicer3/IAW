3129226f919763ef3096b9fcdc5642cc7e5a0051
MiguelPellicer3 <63378@iessierradeguara.com>
2021-01-29 17:14:47 +0100

    29-1

--- node-07-ejs/server.js
+++ node-07-ejs/server.js
@@ -12,13 +12,18 @@ app.get('/', function(req, res) {
     var mascots = [
         { name: 'Sammy', organization: "DigitalOcean", birth_year: 2012},
         { name: 'Tux', organization: "Linux", birth_year: 1996},
-        { name: 'Moby Dock', organization: "Docker", birth_year: 2013}
+        { name: 'Moby Dock', organization: "Docker", birth_year: 2013},
+        { name: 'Tby', organization:"Mi casa", birth_year : 2018},
+        { name: 'KLK', organization:"HUESCONISNG", birth_year : 2000}
     ];
     var tagline = "No programming concept is complete without a cute animal mascot.";
-
+    var miguel = "MIGUEL";
+    let lista = [54,25,65,4,685,65,654,64,65,154,56,5,5];
     res.render('pages/index', {
         mascots: mascots,
-        tagline: tagline
+        tagline: tagline,
+        miguel: miguel,
+        lista: lista,
     });
 });
 
---
+++ node-08/app/index.js
@@ -0,0 +1,18 @@
+const express = require ('express');
+const app = express();
+app.set('puerto', 3000);
+app.set('view engine', 'ejs');
+app.set('views',__dirname+'/vistas');
+
+
+app.get('/',function (req,res) {
+    res.render('paginas/inicio.ejs',{texto:"HOLA MUNDO"});
+});
+
+
+
+
+app.listen(app.get('puerto'),
+function () {
+    console.log(`Que bonico ta quedao jomio http://localhost:${app.get('puerto')}`);
+})
