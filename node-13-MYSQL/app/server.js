c0c02724215929ff7063d3ca347937be7b4cfcea
MiguelPellicer3 <63378@iessierradeguara.com>
2021-02-15 21:08:36 +0100

    node-13

--- node-13-MYSQL/app/server.js
+++ node-13-MYSQL/app/server.js
@@ -1,20 +1,36 @@
-const express= require('express');
-const app= express();
-const path= require('path');
+const express = require('express');
+const dotenv = require('dotenv');
+const app = express();
+const path = require('path');
 const puerto = 3000;
-const mariadb= require('mariadb');
-const datosbd=    {
-    host: 'iesguara.cf',
-    port: 3700,
-    user: 'profeasir1',
-    password: 'profeisa1',
-    database:'profeasir1_Peliculas2',
+const mariadb = require('mariadb');
+
+dotenv.config();//Lee las variables de entrono del fichero .env y las exporta
+
+app.set('view engine', 'ejs');
+app.set('views',__dirname+'/views');
+
+const datosbd = {
+    host: process.env.BD_HOST,
+    port: process.env.BD_PUERTO,
+    user: process.env.BD_USUARIO,
+    password: process.env.BD_PASS,
+    database: process.env.BD_USUARIO + '_' + process.env.BD_SCHEMA,
     connectionLimit: 5
 }
 
-app.get('/', function (req,res) {
-    res.send('Hola mundo');
-    const conexion = mariadb.createPool(datosbd)
+app.get('/', function (req, res) {
+    const grupoConexion = mariadb.createPool(datosbd)
+    grupoConexion.getConnection()
+        .then(conexion => {
+            console.log("Conectado:", conexion.threadId);
+            conexion.query("SELECT * FROM Pelicula")
+                .then(filas => {
+                    res.render('index',{datosTabla:filas});
+                }).catch(error => { console.log(error) });
+            conexion.release();
+        })
+        .catch(error => { console.log("no conectado", error) });
 })
 
 
